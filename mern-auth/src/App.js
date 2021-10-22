aa66a1fff4ca7af7afd8d5ce151c707ba29095ae
sneha <72269836+sneha-del@users.noreply.github.com>
2021-10-22 20:02:26 +0530

    Added about and 404

--- mern-auth/src/App.js
+++ mern-auth/src/App.js
@@ -19,39 +19,41 @@ import Contact from "./components/Pages/Contact";
 import Gallery from "./components/Pages/Gallery";
 import Faq from "./components/Pages/Faq";
 import Doctor from "./components/Pages/Doctor";
+import About from "./components/Pages/About";
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token);
 }
 
 function App() {
-	useEffect(() => {
-		store.dispatch(loadUser());
-	}, []);
-	return (
-		<>
-			<div className="App">
-				<Router>
-					<Navbar/>
-					<Switch>
-						<Route exact path="/landing" component={Landing} />
-						<Route exact path="/" component={Homepage} />
-						<Route exact path="/services" component={Services} />
-						<Route exact path="/blogs" component={Blog} />
-						<Route exact path="/contact-us" component={Contact} />
-						<Route exact path="/gallery" component={Gallery} />
-						<Route exact path="/faq" component={Faq} />
-						<Route exact path="/doctors" component={Doctor} />
-						<PrivateRoute exact path="/dashboard" component={Dashboard} />
-						<Route exact path="/register" component={Register} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="" component={NotFound} />
-					</Switch>
-					<Footer/>
-				</Router>
-			</div>
-		</>
-	);
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/landing" component={Landing} />
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/services" component={Services} />
+            <Route exact path="/blogs" component={Blog} />
+            <Route exact path="/contact-us" component={Contact} />
+            <Route exact path="/gallery" component={Gallery} />
+            <Route exact path="/faq" component={Faq} />
+            <Route exact path="/doctors" component={Doctor} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="" component={NotFound} />
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ mern-auth/src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="about">
+        About the global dialysis or dialysis in general
+      </div>
+    </>
+  );
+};
+
+export default About;
