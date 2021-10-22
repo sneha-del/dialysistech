5a9e1037e4f72d78d30f1e8b0422b1169b4a942c
vikas-narwariya <vickylodhi05@gmail.com>
2021-10-22 12:40:46 +0530

    login logout done

--- mern-auth/src/components/Pages/Navbar.js
+++ mern-auth/src/components/Pages/Navbar.js
@@ -1,7 +1,44 @@
-import React from "react"
+import React , { Fragment } from "react"
 import { Link } from "react-router-dom"
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
 
-const Navbar = () => {
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+
+
+    const authLinks = (
+		<ul className="nav navbar-nav navbar-right">
+			<li>
+				<Link to="/dashboard">
+					<i className="fas fa-user"></i>{" "}
+					<span className="hide-sm">Dashboard</span>
+				</Link>
+			</li>
+			<li>
+				<Link onClick={logout} to="/" replace>
+					<i className="fas fa-sign-out-alt"></i>{" "}
+					<span className="hide-sm"> &nbsp;Logout</span>
+				</Link>
+			</li>
+		</ul>
+	);
+
+    const guestLinks = (
+		<ul className="nav navbar-nav navbar-right">
+            <li>
+				<Link to="/">Home</Link>
+			    </li>
+			<li>
+				<Link to="/register">Register</Link>
+			</li>
+			<li>
+				<Link to="/login">Login</Link>
+			</li>
+		</ul>
+	);
+
     return (
         <section className="navs">
           <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -21,45 +58,11 @@ const Navbar = () => {
               </div>
               {/* Collect the nav links, forms, and other content for toggling */}
               <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav navbar-right">
-                <li>
-				<Link to="/">Home</Link>
-			    </li>
-                <li>
-				<Link to="/register">Register</Link>
-			    </li>
-                <li>
-				<Link to="/login">Login</Link>
-			    </li>
-                {/* <li>
-				<Link to="/services">Services</Link>
-			    </li>
-                <li>
-				<Link to="/doctors">Doctor</Link>
-			    </li>
-                <li>
-				<Link to="/blogs">Blog</Link>
-			    </li>
-                <li>
-				<Link to="/faq">Faq</Link>
-			    </li>
-                <li>
-				<Link to="/gallery">Gallery</Link>
-			    </li> */}
-                {/* <li>
-				<Link to="/">Home</Link>
-			    </li>
-                <li>
-				<Link to="/">Home</Link>
-			    </li>
-                  <li><a href="index.html">Home</a></li>
-                  <li><a href="services.html">Services</a></li>
-                  <li><a href="doctors.html">Doctors</a></li>
-                  <li><a href="blog.html">Blog</a></li>
-                  <li><a href="faq.html">FAQs</a></li>
-                  <li><a href="gallery.html">Gallery</a></li>
-                  <li className="active"><a href="#">Contact Us</a></li> */}
-                </ul>
+              
+			{!loading && (
+				<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+			)}
+               
               </div>{/* /.navbar-collapse */}
             </div>{/* /.container-fluid */}
           </nav>
@@ -67,4 +70,13 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+Navbar.propTypes = {
+	logout: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
