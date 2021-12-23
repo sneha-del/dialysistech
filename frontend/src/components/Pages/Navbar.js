3ca8fc3c0a65cd2069b19d711e28095e6bccc658
sneha <72269836+sneha-del@users.noreply.github.com>
2021-12-23 22:31:39 +0530

    logout btn added

--- frontend/src/components/Pages/Navbar.js
+++ frontend/src/components/Pages/Navbar.js
@@ -1,71 +1,113 @@
-import React , { Fragment } from "react"
-import { Link } from "react-router-dom"
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
 
-
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/services">
+          Services
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/doctor">
+          Doctor
+        </Link>
+      </li>
 
+      <li className="nav-item">
+        <Link className="nav-link" to="/contact-us">
+          Contact
+        </Link>
+      </li>
 
-    const authLinks = (
-		<ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/dashboard">
+          <i className="fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li className="nav-item">
+        {/* <div className="btn-container ">
+                    <Link to="/appointment" className="btn btn-main-2 btn-icon btn-round-full">Make appoinment <i className="icofont-simple-right ml-2  " /></Link>
+                  </div> */}
+        {/* <div className="btn-container"> */}
+        <Link
+          onClick={logout}
+          className="btn btn-main btn-round-full"
+          to="/"
+          replace
+        >
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm"> &nbsp;Logout</span>
+        </Link>
+        {/* </div> */}
+      </li>
+    </ul>
+  );
 
-			<li className="nav-item"><Link className="nav-link" to="/">
-				Home</Link>
-			    </li>
-				<li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
-			  <li className="nav-item"><Link className="nav-link" to="/doctor">Doctor</Link></li>
-           
-              <li className="nav-item"><Link className="nav-link" to="/contact-us">Contact</Link></li>
-			  
-			  <li className="nav-item">
-				<Link className="nav-link" to="/dashboard">
-					<i className="fas fa-user"></i>{" "}
-					<span className="hide-sm">Dashboard</span>
-				</Link>
-			</li>
-			<li className="nav-item">
-				<Link onClick={logout} className="nav-link" to="/" replace>
-					<i className="fas fa-sign-out-alt"></i>{" "}
-					<span className="hide-sm"> &nbsp;Logout</span>
-				</Link>
-			</li>
-					  </ul>
-	);
+  const guestLinks = (
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/services">
+          Services
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/doctor">
+          Doctor
+        </Link>
+      </li>
 
-    const guestLinks = (
-		<ul className="navbar-nav ml-auto">
-            <li className="nav-item"><Link className="nav-link" to="/">
-				Home</Link>
-			    </li>
-				<li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
-			  <li className="nav-item"><Link className="nav-link" to="/doctor">Doctor</Link></li>
-             
-              
-              
-              <li className="nav-item"><Link className="nav-link" to="/contact-us">Contact</Link></li>
-			  
-              <li className="nav-item"><Link className="btn btn-main btn-round-full" to="/register">Signup</Link></li>
-		</ul>
-	);
+      <li className="nav-item">
+        <Link className="nav-link" to="/contact-us">
+          Contact
+        </Link>
+      </li>
 
-    return (
+      <li className="nav-item">
+        <Link className="btn btn-main btn-round-full" to="/register">
+          Signup
+        </Link>
+      </li>
+    </ul>
+  );
 
-<>
-		<nav className="navbar navbar-expand-lg navigation" id="navbar">
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navigation" id="navbar">
         <div className="container">
           <a className="navbar-brand" href="index.html">
             <img src="images/logo.png" alt="" className="img-fluid" />
           </a>
-          <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarmain"
+            aria-controls="navbarmain"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="icofont-navigation-menu" />
           </button>
           <div className="collapse navbar-collapse" id="navbarmain">
-
-		  {!loading && (
-				<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-			)}
+            {!loading && (
+              <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+            )}
             {/* <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
                 <Link className="nav-link" to="/">Home</Link>
@@ -82,7 +124,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
           </div>
         </div>
       </nav>
-        {/* <section className="navs">
+      {/* <section className="navs">
           <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
             <div className="container-fluid">
               <div className="navbar-header">
@@ -107,17 +149,17 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
             </div>
           </nav>
         </section> */}
-		</>
-    )
-}
+    </>
+  );
+};
 
 Navbar.propTypes = {
-	logout: PropTypes.func.isRequired,
-	auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	auth: state.auth,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logout })(Navbar);
