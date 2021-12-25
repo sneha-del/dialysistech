c9c021f2b6af675cb4115691f02816d10a380b81
Vikas Narwariya <vickylodhi05@gmail.com>
2021-12-25 06:05:28 +0530

    warnings removed

--- index.js
+++ index.js
@@ -39,13 +39,14 @@ app.use("/users", require("./routes/auth"));
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('mern-auth/build'));
+  app.use(express.static('frontend/build'));
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./mern-auth", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./frontend", "build", "index.html"));
 });
 }
+
 
 // mongoose
 //   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
